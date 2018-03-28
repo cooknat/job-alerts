723111e233140fa5f253568e5b58f128e8e35c12
cooknat <natalie.t.cook@gmail.com>
2018-03-28 14:33:10 +0100

    update job page

--- app/controllers/job_pages_controller.rb
+++ app/controllers/job_pages_controller.rb
@@ -1,6 +1,6 @@
 class JobPagesController < ApplicationController
  
-   before_action :authorise_user, only: [:edit, :destroy]
+   before_action :authorise_user, only: [:update, :destroy]
 
   def index
     @jobPages = JobPage.all
@@ -30,7 +30,21 @@ class JobPagesController < ApplicationController
   end  
 
   def edit
-    #leave for now, should only be available to site admin
+    @jobPage = JobPage.find(params[:id])
+  end  
+
+  def update
+    @jobPage = JobPage.find(params[:id])
+    @jobPage.name = params[:job_page][:name]
+    @jobPage.url = params[:job_page][:url]
+
+     if @jobPage.save
+        flash[:notice] = "Job page was saved."       
+      redirect_to job_pages_path
+    else
+      flash.now[:alert] = "There was an error saving your changes. Please try again."
+      render :edit
+    end
   end
 
   def destroy
--- app/helpers/job_pages_helper.rb
+++ app/helpers/job_pages_helper.rb
@@ -1,2 +1,5 @@
 module JobPagesHelper
+  def user_is_authorised_to_delete?
+        current_user && current_user.admin?
+   end
 end
